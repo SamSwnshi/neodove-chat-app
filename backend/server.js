84154c82c6fc3998e47dadf7733beff57ab40b33
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-07-11 11:52:38 +0530

    build completed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from 'dotenv'
 import connectDb from "./db/config.js";
@@ -7,12 +8,12 @@ import messageRoutes from "./routes/message.routes.js"
 import cookieParser from "cookie-parser";
 import { server,app } from "./socket/socket.js"
 
+const port = process.env.PORT || 8000;
+
+const __dirname = path.resolve()
 
 dotenv.config()
 
-
-
-const port = process.env.PORT || 8000;
 
 app.use(express.json())
 app.use(cookieParser())
@@ -21,8 +22,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 app.use("/api/users",userRoutes)
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
 });
 
 
